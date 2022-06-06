918d4eceeb70f90a6759b8a3213ca16852e13eae
ssuish <fubuki.k016@gmail.com>
2022-06-06 10:42:31 +0800

    new

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 from asyncio import proactor_events
+from pydoc import cli
 import discord as dc
 import logging as log
 from private import Secret
@@ -6,9 +7,16 @@ from private import Secret
 tk = Secret().get_token()
 client = dc.Client()
 
+# client.events
 @client.event
 async def on_ready():
     print('Logged in as {0.user}' .format(client))
+    
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
 @client.event
 async def on_message(message):
@@ -16,6 +24,15 @@ async def on_message(message):
         return
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+# bot events
+@client.event
+async def on_guild_join(guild):
+    print('Bot has been added to a new server')
+    print('List of servers the bot is in: ')
+
+    for guild in client.guilds:
+        await print(guild.name)
 
 # Log file
 logger = log.getLogger('discord')
