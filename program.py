2e11e45c8b9a83ab908ab71a930f05660ba1283e
ssuish <fubuki.k016@gmail.com>
2022-06-07 22:18:06 +0800

    initial commit for math-lexer

--- program.py
+++ program.py
@@ -2,36 +2,48 @@ import discord as dc
 import logging as log
 from private import Secret
 
-tk = Secret.TOKEN
 client = dc.Client()
-prefix = "s!"
 
+class SsuishEvents:    
+    prefix = "s!"
 
-# client.events
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}' .format(client))
+    @client.event
+    async def on_ready():
+        print('Logged in as {0.user}' .format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith(prefix + 'hello'):
-        await message.channel.send('Hello!')
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        if message.content.startswith(SsuishEvents.prefix + 'hello'):
+            await message.channel.send('Hello!')
 
-# bot events
-@client.event
-async def on_guild_join(guild):
-    print('Bot has been added to a new server')
-    print('List of servers the bot is in: ')
+    @client.event
+    async def on_guild_join(guild):
+        print('Bot has been added to a new server')
+        print('List of servers the bot is in: ')
+        for guild in client.guilds:
+            await print(guild.name)
 
-    for guild in client.guilds:
-        await print(guild.name)
+class SsuishLogs:
+    def log_setup():
+        logger = log.getLogger('discord')
+        logger.setLevel(log.DEBUG)
+        handler = log.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+        handler.setFormatter(log.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+        logger.addHandler(handler)
 
-# Log file
-logger = log.getLogger('discord')
-logger.setLevel(log.DEBUG)
-handler = log.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(log.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-client.run(tk)
+class Main:
+    def __init__(self):
+        SsuishLogs.log_setup()
+        Main.main()
+        SsuishEvents.on_ready()
+        SsuishEvents.on_message()
+        SsuishEvents.on_guild_join()
+    
+    def main():
+        tk = Secret.TOKEN
+        client.run(tk)
+    
+if __name__ == '__main__':
+    Main.main()
