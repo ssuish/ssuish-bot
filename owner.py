fb897a088cd508549b0b35d060de3e4a452b108a
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-08 14:35:43 +0800

    Delete owner.py
    leaked

--- owner.py
+++
@@ -1,8 +0,0 @@
-class Secret:
-    TOKEN = "ODkwMTM5NzEwODg0ODE4OTg0.Gh3IEP.PZDYMqkeMu36WMf-YP9UTSFfbadxanP6kNqizQ"
-    
-    def __init__(self):
-        self.TOKEN = Secret.TOKEN
-        
-    def get_token(self):
-        return self.TOKEN
