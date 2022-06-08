095c8fc091ae0a7074749caf0b1df1a7672d8bdb
ssuish <fubuki.k016@gmail.com>
2022-06-08 12:58:57 +0800

    rename

--- Interpreter/Lexer-1/shell.py
+++ Interpreter/Lexer-1/shell.py
@@ -1,7 +1,7 @@
 import basic
 
 while (True):
-    text = input('ssuish> ')
+    text = input('ssuish > ')
     result, error = basic.run('<stdin>', text)
     
     if error: print(error.as_string())
