bf56dde743b3a4dad8629a6d9993e7ee09812da9
suisoh <88411618+ssuish@users.noreply.github.com>
2022-12-30 22:25:42 +0800

    woking bot 1

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import os
 from private import token
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print("We log in as ", client)
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$Hi"):
-        await message.channel.send("Hi")
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(token.get_token())
