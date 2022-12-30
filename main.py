9a31b72785773f37773106188fb6d669f231314d
suisoh <88411618+ssuish@users.noreply.github.com>
2022-12-30 21:57:21 +0800

    new cmd

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
+    if message.content.startswith("$Hi"):
         await message.channel.send("Hi")
 
 client.run(token.get_token())
